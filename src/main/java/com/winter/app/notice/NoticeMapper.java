9f8b70628acc62efdcc5a4d37af1225fc3e55dcc
ShinJeongho <sinnago_jh@naver.com>
2024-09-03 09:31:38 +0900

    sa

--- src/main/java/com/winter/app/notice/NoticeMapper.java
+++ src/main/java/com/winter/app/notice/NoticeMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface NoticeMapper {
 	// 게시물 목록을 가져오는 메서드
-	public List<NoticeVO> getList()throws Exception;
+	public List<NoticeVO> getList(Long num)throws Exception;
 	// 게시물을 삽입하는 메서드
 	public int insertNotice(NoticeVO noticeVO)throws Exception;
 
